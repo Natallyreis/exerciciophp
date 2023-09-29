74484d3fde48d0673a54b5d5832a3935140ca23b
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-09-29 12:01:10 -0300

    commit

--- ex4.php
+++ ex4.php
@@ -4,12 +4,12 @@ $velocidade = 45;
 
 // Verifique a velocidade usando uma estrutura IF
 if ($velocidade >= 41 && $velocidade <= 50) {
-    echo "O motorista está na velocidade correta.";
+    echo " <br> O motorista está na velocidade correta.";
 } elseif ($velocidade == 40) {
-    echo "Atenção: Você está na velocidade limite permitida.";
+    echo " <br> Atenção: Você está na velocidade limite permitida.";
 } elseif ($velocidade > 50) {
-    echo "Você está acima do limite de velocidade. Multa aplicada.";
+    echo " <br> Você está acima do limite de velocidade. Multa aplicada.";
 } else {
-    echo "Você está abaixo do limite de velocidade permitida.";
+    echo " <br> Você está abaixo do limite de velocidade permitida.";
 }
 ?>
