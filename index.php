cebd805903d1cba504a4d09fc06ea1cf03ffa7c6
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-09-29 09:09:47 -0300

    commit

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
 <?php
+    $num1 = 5;
+    $num2 = 2;
+    
+if($num1 > $num2) {
+    echo "Verdadeiro";
+} else {
+    echo "Falso";
+}
+
+    $robson = 1;
+    $pedro = 2;
+
+if($robson != $pedro){
+    echo "Robson é diferente de Pedro";
+} else {
+    echo "Robson é igual a Pedro";
+}
+
+    $num1 = 12;
+    $num2 = 11;
+
+
+if($num1 <= $num2 ) {
+    echo "Doze não é menor ou igual a onze";
+} else {
+    echo "Doze é menor ou igual a onze";
+}
+?>
